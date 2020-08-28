33e02076541f7c59e28bc616746768576a028bfc
lucashulej <lucashulej@gmail.com>
2020-08-27 22:11:40 -0300

    Ejercicio 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-first-project';
+  edadUno: number;
+  edadDos: number;
+  promedio: number = 0;
+  suma:number = 0;
+
+  calcular() {
+    this.suma = this.edadUno + this.edadDos;
+    this.promedio = this.suma / 2;
+  }
+
+  limpiar() {
+    this.edadUno = null;
+    this.edadDos = null;
+    this.suma = 0;
+    this.promedio = 0;
+  }
 }
